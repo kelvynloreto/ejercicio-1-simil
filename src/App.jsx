c5494812f935fa5c5d02b5c5debe908ceac7e4cf
kelvynloreto <loretokelvyn@gmail.com>
2022-07-22 09:43:07 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import DataUsers from "./components/DataUsers.jsx";
+
+
+function App() {
+ 
+
+  return (
+<>
+   <DataUsers/> 
+    
+   </>
+   
+  );
+}
+
+export default App;
---
+++ src/components/DataUsers.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import data from "../json/data.json";
+
+const DataUsers = () => {
+  const random = Math.floor(Math.random() * data.length);
+  const auxObj = data[Math.floor(Math.random() * data.length)];
+  const { large } = auxObj.picture;
+  const { title, first, last } = auxObj.name;
+  const email = auxObj.email;
+  const phone = auxObj.phone;
+  const { city, state, country } = auxObj.location;
+
+  const [shuffle, setshuffle] = useState(0);
+  const randomUser = () => {
+    setshuffle(Math.floor(Math.random() * data.length));
+    console.log(random);
+  };
+  return (
+    <div className={`App bg${random}`}>
+      <div className={`cards_Users card${random}`}>
+        <h1>{`${title} ${first} ${last} `}</h1>
+        <img src={large} alt="" />
+        <div className="box_icons">
+          <box-icon name="envelope" color={`var(--Color${random})`}></box-icon>
+          <p>{`${email} `}</p>
+        </div>
+        <div className="box_icons">
+          <box-icon name="phone" color={`var(--Color${random})`}></box-icon>
+          <p>{`${phone} `}</p>
+        </div>
+        <div className="box_icons">
+          <box-icon name="map" color={`var(--Color${random})`}></box-icon>
+          <p>{`${city} ${state} ${country} `}</p>
+        </div>
+        <div className={`iconrepeat bg${random}`}>
+          <box-icon
+            onClick={randomUser}
+            className="repeat"
+            name="repeat"
+            animation="tada"
+          ></box-icon>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DataUsers;
